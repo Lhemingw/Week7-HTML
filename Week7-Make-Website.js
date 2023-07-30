27299f68c8a0c63b557572285271dc79d26e9820
Lhemingw <lhemingw@gmail.com>
2023-07-30 17:01:11 -0600

    first commit

---
+++ Week7-Make-Website.js
@@ -0,0 +1,18 @@
+
+//Instructions:
+//     In Visual Studio Code, write the code that accomplishes the objectives listed below and ensures that the code compiles and runs as directed.
+//     Comment your code, to prove that you understand what you have written -- this is required!
+//     Create a new repository on GitHub for this week's assignments and push your code to the repository.
+//     Record and Upload a Video showcasing your assignment.
+//     Keep Track of the URLs for this week's GitHub repository and Video upload.
+//     Start the Quiz to submit your assignment.
+//         Put the URL for your GitHub repo into the Quiz
+//         Put the URL for your Video into the Quiz
+//         Answer the questions
+
+// Coding Steps:
+
+//     Create a website using only HTML. The website can be anything you like (within school guidelines) if it meets the following criteria:
+//         At least 3 pages with links to navigate between all pages present on each page.
+//         2 different size headers, a list, 3 images, a table, and a form.
+
